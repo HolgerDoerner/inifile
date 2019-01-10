aa5f919c9564b1b7e828a4a3d181871f51a003d4
HolgerDoerner <holger.doerner@gmail.com>
2019-01-10 10:06:56 +0100

    updated javadoc

--- IniFile/src/com/inifile/DuplicateEntryException.java
+++ IniFile/src/com/inifile/DuplicateEntryException.java
@@ -1,6 +1,9 @@
 package com.inifile;
 
 /**
+ * Indicates that an Dulicate of a Section or a Key inside of a Section
+ * was found in the File.
+ * 
  * @author Holger Dörner
  * @version 1.0
  * @see java.lang.RuntimeException *
